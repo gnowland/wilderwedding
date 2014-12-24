32be2c048612ba8cbeb848f59d59b50271b4a966
Gifford Nowland <gnowland@gmail.com>
2014-12-24 02:13:20 -0600

    disabled demo CPT

--- wp-content/themes/wilderwedding/functions.php
+++ wp-content/themes/wilderwedding/functions.php
@@ -8,7 +8,7 @@ INCLUDE NEEDED FILES
 require_once(get_template_directory().'/library/joints.php'); 
 
 // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
-require_once(get_template_directory().'/library/custom-post-type.php'); // you can disable this if you like
+//require_once(get_template_directory().'/library/custom-post-type.php'); // you can disable this if you like
 
 // CUSTOMIZE THE WORDPRESS ADMIN (off by default)
 // require_once(get_template_directory().'/library/admin.php'); 
