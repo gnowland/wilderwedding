e64431609bf5ba638c6b97ee719617ae9f56f507
Gifford Nowland <gnowland@gmail.com>
2015-01-11 01:07:02 -0600

    updated favicons

--- wp-content/themes/wilderwedding/header.php
+++ wp-content/themes/wilderwedding/header.php
@@ -1,6 +1,9 @@
 <!doctype html>
 
-  <html class="no-js"  <?php language_attributes(); ?>>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 
 	<head>
 		<meta charset="utf-8">
@@ -13,55 +16,49 @@
 			else { bloginfo('name'); wp_title('-', true,'left'); }
 		?></title>
 
-
 		<!-- Mobile Meta -->
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="width">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<!-- favicons -->
-		<link rel="shortcut icon"                                   href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<link rel="icon"                  sizes="16x16 32x32 64x64" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<link rel="icon" type="image/png" sizes="196x196"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-196.png">
-		<link rel="icon" type="image/png" sizes="160x160"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-160.png">
-		<link rel="icon" type="image/png" sizes="96x96"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-96.png">
-		<link rel="icon" type="image/png" sizes="64x64"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-64.png">
-		<link rel="icon" type="image/png" sizes="32x32"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-32.png">
-		<link rel="icon" type="image/png" sizes="16x16"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-16.png">
-		<link rel="apple-touch-icon"      sizes="152x152"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-152.png">
-		<link rel="apple-touch-icon"      sizes="144x144"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-144.png">
-		<link rel="apple-touch-icon"      sizes="120x120"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-120.png">
-		<link rel="apple-touch-icon"      sizes="114x114"           href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-114.png">
-		<link rel="apple-touch-icon"      sizes="76x76"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-76.png">
-		<link rel="apple-touch-icon"      sizes="72x72"             href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-72.png">
-		<link rel="apple-touch-icon"                                href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-57.png">
-		<meta name="msapplication-TileColor" content="#152348">
-		<meta name="msapplication-TileImage"                     content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-144.png">
-		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/browserconfig.xml">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=2">
+		<link rel="icon" sizes="16x16 32x32 64x64" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=2">
+		<link rel="icon" type="image/png" sizes="196x196" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-192.png?v=2">
+		<link rel="icon" type="image/png" sizes="160x160" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-160.png?v=2">
+		<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-96.png?v=2">
+		<link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-64.png?v=2">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-32.png?v=2">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-16.png?v=2">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-180.png?v=2">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-152.png?v=2">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-144.png?v=2">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-120.png?v=2">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-114.png?v=2">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-76.png?v=2">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-72.png?v=2">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-60.png?v=2">
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-57.png?v=2">
+		<meta name="msapplication-TileColor" content="#022644">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-144.png?v=2">
+		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/browserconfig.xml?v=2">
+		<!--[if IE]> <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico"> <![endif]-->
 
-		<meta name="theme-color" content="#152348">
+		<meta name="theme-color" content="#022644">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<?php wp_head(); ?>
 
-		<!-- Drop Google Analytics here -->
-		<!-- end analytics -->
-
 	</head>
 
-	<body <?php body_class(); ?>>
-		<div class="off-canvas-wrap" data-offcanvas>
+	<body <?php body_class(); ?>> <!-- Closes in footer.php -->
+		<div class="off-canvas-wrap" data-offcanvas> <!-- Closes in footer.php -->
 			<div class="inner-wrap">
 				<div id="container">
 					<header class="header" role="banner">
 
-						 <!-- This navs will be applied to the topbar, above all content
-							  To see additional nav styles, visit the /partials directory -->
-						 <?php get_template_part( 'partials/nav', 'top-offcanvas' ); ?>
+						<?php // get_template_part( 'partials/nav', 'top-offcanvas' ); ?>
 
 						<div id="inner-header" class="row">
 							<div class="large-12 medium-12 columns">
@@ -75,10 +72,7 @@
 								</h1>
 							</div>
 
-							 <!-- This navs will be applied to the main, under the logo
-								  To see additional nav styles, visit the /partials directory -->
-
-							 <?php // get_template_part( 'partials/nav', 'main-offcanvas' ); ?>
+						<?php // get_template_part( 'partials/nav', 'main-offcanvas' ); ?>
 
 						</div> <!-- end #inner-header -->
 					</header> <!-- end .header -->
