1d371a43a74e584fb31cf2cfccd2e7908522792f
Gifford Nowland <gnowland@gmail.com>
2015-01-11 17:49:31 -0600

    re-enabled off canvas ability (oops)

--- wp-content/themes/wilderwedding/footer.php
+++ wp-content/themes/wilderwedding/footer.php
@@ -1,15 +1,18 @@
-		<footer class="footer" role="contentinfo">
-			<div id="inner-footer" class="row">
-				<div class="large-12 medium-12 columns">
-					<nav role="navigation">
-  						<?php joints_footer_links(); ?>
-  					</nav>
-  				</div>
-                <div class="large-12 medium-12 columns">
-                <p class="source-org copyright">&copy; <?php echo date('Y').' '; bloginfo('description'); ?><br>Design+Development by <a href="http://www.giffordnowland.com/" target="_blank">Gifford Nowland</a></p>
-				</div>
-			</div> <!-- end #inner-footer -->
-		</footer> <!-- end .footer -->
+					<footer class="footer" role="contentinfo">
+						<div id="inner-footer" class="row">
+							<div class="large-12 medium-12 columns">
+								<nav role="navigation">
+		    						<?php joints_footer_links(); ?>
+		    					</nav>
+		    				</div>
+			                <div class="large-12 medium-12 columns">
+			                <p class="source-org copyright">&copy; <?php echo date('Y').' '; bloginfo('description'); ?><br>Design+Development by <a href="http://www.giffordnowland.com/" target="_blank">Gifford Nowland</a></p>
+							</div>
+						</div> <!-- end #inner-footer -->
+					</footer> <!-- end .footer -->
+				</div> <!-- end #container -->
+			</div> <!-- end .inner-wrap -->
+		</div> <!-- end .off-canvas-wrap -->
 		<?php
 			// all js scripts are loaded in library/joints.php
 			wp_footer();
