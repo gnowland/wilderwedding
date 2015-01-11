3c145d7e93b49416a3e7ac42b2d80cf7e00166f7
Gifford Nowland <gnowland@gmail.com>
2015-01-11 17:18:24 -0600

    edited footer copy

--- wp-content/themes/wilderwedding/footer.php
+++ wp-content/themes/wilderwedding/footer.php
@@ -5,14 +5,16 @@
 		    						<?php joints_footer_links(); ?>
 		    					</nav>
 		    				</div>
-			                <div class="large-12 medium-12 columns">		
-								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-							</div>		
-						</div> <!-- end #inner-footer -->			
+			                <div class="large-12 medium-12 columns">
+			                <p class="source-org copyright">&copy; <?php echo date('Y').' '; bloginfo('description'); ?><br>Design+Development by <a href="http://www.giffordnowland.com/" target="_blank">Gifford Nowland</a></p>
+							</div>
+						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
 				</div> <!-- end #container -->
 			</div> <!-- end .inner-wrap -->
-		</div> <!-- end .off-canvas-wrap -->		
-		<!-- all js scripts are loaded in library/joints.php -->
-		<?php wp_footer(); ?>
+		</div> <!-- end .off-canvas-wrap -->
+		<?php
+			// all js scripts are loaded in library/joints.php
+			wp_footer();
+		?>
 	</body>
